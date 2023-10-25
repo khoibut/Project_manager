736767ca75ea0b293cd59fbc56c31fb17dec8c7f
khoibut <khoibut09@gmail.com>
2023-10-25 09:13:00 +0700

    Make windows start pos static

--- historyWindow.js
+++ historyWindow.js
@@ -2,6 +2,8 @@ let closeHistoryButton=document.querySelector(".button-close-window-history")
 let openHistoryButton=document.querySelector(".History")
 openHistoryButton.addEventListener("click",function(){
     document.getElementsByClassName("window-history")[0].style.display="grid"
+    document.getElementsByClassName("window-history")[0].style.top=''
+    document.getElementsByClassName("window-history")[0].style.left=''
 })
 closeHistoryButton.addEventListener("click",function(){
     document.getElementsByClassName("window-history")[0].style.display="none"
