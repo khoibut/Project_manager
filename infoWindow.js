454ed8b219c2f768437fba7b5be100404fe737a5
khoibut <khoibut09@gmail.com>
2023-10-26 21:43:10 +0700

    Added info window

--- infoWindow.js
+++ infoWindow.js
@@ -1,8 +1,11 @@
 function openInfo(project){
     let infoWindow=document.querySelector(".window-information")
     infoWindow.style.display="grid"
+    console.log(project)
     onTop(document.querySelector("[data-button]").parentElement)
-    infoWindow.querySelector(".link").setAttribute("href",project.querySelector("[project-URL]").innerHTML)
+    infoWindow.querySelector(".link").setAttribute("href",project.querySelector("[data-src]").innerHTML)
+    infoWindow.querySelector(".project-name").innerHTML=project.querySelector(".project-name").innerHTML
+    infoWindow.querySelector(".picture").style.background=project.querySelector("[data-display]").innerHTML
 }
 {
     let infoWindow=document.querySelector("[data-button]")
